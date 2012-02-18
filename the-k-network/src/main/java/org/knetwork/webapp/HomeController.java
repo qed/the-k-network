f332c024aa7bf81cfcffd0a66a8f7250be168bce
Hamed Asghari <hasghari@gmail.com>
2012-02-18 10:47:13 -0600

    reverting to login home page

--- the-k-network/src/main/java/org/knetwork/webapp/HomeController.java
+++ the-k-network/src/main/java/org/knetwork/webapp/HomeController.java
@@ -12,10 +12,12 @@ import org.scribe.model.OAuthConstants;
 import org.scribe.model.Token;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
--- the-k-network/src/main/java/org/knetwork/webapp/controller/LearningController.java
+++ the-k-network/src/main/java/org/knetwork/webapp/controller/LearningController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+//@Controller
 public class LearningController {
     
     @Value("${twiddla.username}")
