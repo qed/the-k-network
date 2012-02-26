5fb2e844e440167f3ecdf984470b35c75dac2921
Chris <christophercoydev@gmail.com>
2012-02-26 13:03:37 -0600

    Adding session map util

--- the-k-network/src/main/java/org/knetwork/webapp/util/SessionMapUtil.java
+++ the-k-network/src/main/java/org/knetwork/webapp/util/SessionMapUtil.java
@@ -11,7 +11,7 @@ public class SessionMapUtil {
 	protected static Map<String, Map<String, String>> sessionMap = new Hashtable<String, Map<String, String>>();
 	
 	public static void addLearningSessionToMap(String learningSessionId) {
-		if(sessionMap.get(learningSessionId)==null) {
+		if(!isLearningSessionMapped(learningSessionId)) {
 			Map<String, String> apiSessionMap = new Hashtable<String, String>();
 			apiSessionMap.put(WHITEBOARD, "");
 			apiSessionMap.put(TOKBOX, "");
@@ -19,7 +19,33 @@ public class SessionMapUtil {
 		}
 	}
 	
+	private static boolean isLearningSessionMapped(String learningSessionId) {
+		return sessionMap.get(learningSessionId)!=null;
+	}
+	
+	public static boolean tokboxSessionExists(String learningSessionId) {
+		if(!isLearningSessionMapped(learningSessionId)) return false;
+		
+		return getTokboxSessionId(learningSessionId) != null;
+	}
+	
 	public static String getTokboxSessionId(String learningSessionId) {
-		return null;
+		if(!isLearningSessionMapped(learningSessionId)) {
+			addLearningSessionToMap(learningSessionId);
+			return null;
+		} else {
+			if(tokboxSessionExists(learningSessionId)) {
+				return sessionMap.get(learningSessionId).get(TOKBOX);
+			} else {
+				return null;
+			}
+		}
+	}
+	
+	public static void addTokboxSessionId(String learningSessionId, String tokboxSessionId) {
+		if(!tokboxSessionExists(learningSessionId)) {
+			sessionMap.get(learningSessionId).put(TOKBOX, tokboxSessionId);
+		}
+		
 	}
 }
